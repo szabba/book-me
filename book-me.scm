a16cda07ff212617e7e3c30f624f38fcdf0855a8
Karol Marcjan <karol.marcjan@gmail.com>
2013-07-16 23:12:55 +0200

    Move the License section up to the front

--- book-me.scm
+++ book-me.scm
@@ -15,6 +15,21 @@
 ;;; Gorrie](http://github.org/lukego), it can be run on itself. Neat,
 ;;; huh?
 ;;;
+;;; # License
+;;;
+;;; This program is free software: you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation, either version 3 of the License, or
+;;; (at your option) any later version.
+;;;
+;;; This program is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;
 ;;; # Imports
 ;;;
 ;;; R7RS introduced a new library system. It is by design incompatible
@@ -158,18 +173,3 @@
 ;;; `pandoc` is pretty good at producing PDFs out of Markdown. It adds
 ;;; some extensions of it's own to the vanilla format. Try it -- you'll
 ;;; have a printable version of your code!
-;;;
-;;; # License
-;;;
-;;; This program is free software: you can redistribute it and/or modify
-;;; it under the terms of the GNU General Public License as published by
-;;; the Free Software Foundation, either version 3 of the License, or
-;;; (at your option) any later version.
-;;;
-;;; This program is distributed in the hope that it will be useful,
-;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;; GNU General Public License for more details.
-;;;
-;;; You should have received a copy of the GNU General Public License
-;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
